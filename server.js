86d2421b1af51fc3270c47cf7d1c6a503411decb
Rene Santiago <rsj510@gmail.com>
2022-11-30 19:54:39 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 4321
+
+app.get('/', (request, response) => {
+    response.send(`hello world i am listening to port ${PORT}`);
+});
+
+
+app.get('/dburl', (req, res) => {
+    res.send(`My connection string is: ${process.env.DATABASE_URL}`)
+})
+
+app.listen(PORT, () => {
+    console.log(`PORT: ${PORT} 🌟`)
+})
